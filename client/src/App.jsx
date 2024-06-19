b56a0e70b0cb889252b4fab71549460165b4867b
unknown <arvincabrera37@gmail.com>
2024-06-19 14:39:51 +0800

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import Header  from './components/header';
+import "./assets/css/header.css";
+function App() {
+  return (
+    <>
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/header.jsx
@@ -0,0 +1,13 @@
+function Header(){
+    return(
+    <>
+        <ul>
+            <li><a href="">How to study together</a></li>
+            <li><a href="">Community</a></li>
+            <li><a href="">About</a></li>
+        </ul>
+    </>
+    )
+}
+
+export default Header;
