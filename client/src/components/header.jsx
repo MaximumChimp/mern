ceb519e2c8778499a948d61c386693a5ee0c9caf
unknown <arvincabrera37@gmail.com>
2024-06-19 14:57:48 +0800

    How to study together to Home

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -2,8 +2,8 @@ function Header(){
     return(
     <>
         <ul>
-            <li><a href="">How to study together</a></li>
-            <li><a href="">Community</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">New</a></li>
             <li><a href="">About</a></li>
         </ul>
     </>
